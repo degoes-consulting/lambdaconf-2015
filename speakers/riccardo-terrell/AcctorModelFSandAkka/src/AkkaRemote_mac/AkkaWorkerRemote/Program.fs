d0477fa40c6f392568ccccd0554583b649f7cf17
Riccardo <tericcardo@gmail.com>
2015-05-17 00:29:24 -0400

    The Actor Model in F# and Akka.NET

---
+++ speakers/riccardo-terrell/AcctorModelFSandAkka/src/AkkaRemote_mac/AkkaWorkerRemote/Program.fs
@@ -0,0 +1,53 @@
+﻿open Akka
+open Akka.FSharp
+open Akka.Configuration
+open System
+
+[<EntryPoint>]
+let main argv =
+
+    Console.Title <- sprintf "Remote Worker Actor - PID %d" (System.Diagnostics.Process.GetCurrentProcess().Id)
+    
+    let config = 
+        Configuration.parse """
+        akka {  
+            log-config-on-start = on
+            stdout-loglevel = DEBUG
+            loglevel = DEBUG
+            actor {
+                provider = "Akka.Remote.RemoteActorRefProvider, Akka.Remote"
+            }
+            remote {
+                helios.tcp {
+                    transport-class = "Akka.Remote.Transport.Helios.HeliosTcpTransport, Akka.Remote"
+                    transport-protocol = tcp
+                    port = 8091
+                    hostname = "10.211.55.2"
+                }
+            }
+          } 
+        """ 
+
+    let config' = Configuration.parse """
+        akka {
+            log-config-on-start = on
+            stdout-loglevel = DEBUG
+            loglevel = DEBUG
+
+            actor.provider = "Akka.Remote.RemoteActorRefProvider, Akka.Remote"
+            remote.helios.tcp {
+                hostname = 10.211.55.2
+                port = 8091
+            }
+        }"""
+          
+    use system = System.create "worker" config'
+
+    Console.ForegroundColor <- ConsoleColor.Red
+
+    printfn "Remote Worker Actor %s listening..." system.Name
+
+    Console.ForegroundColor <- ConsoleColor.Green
+
+    Console.ReadLine() |> ignore
+    0
---
+++ speakers/riccardo-terrell/AcctorModelFSandAkka/src/AkkaRemote_mac/Manelbrot/MyClass.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Drawing;
+
+namespace MandelbrotSet
+{
+	public static class Mandelbrot
+	{
+		public static Bitmap Set(int xp, int yp, int w, int h, int width, int height, double maxr, double minr, double maxi, double mini)
+		{
+			Bitmap img = new Bitmap(w, h);
+			double zx = 0;
+			double zy = 0;
+			double cx = 0;
+			double cy = 0;
+			double xjump = ((maxr - minr) / Convert.ToDouble(width));
+			double yjump = ((maxi - mini) / Convert.ToDouble(height));
+			double tempzx = 0;
+			int loopmax = 1000;
+			int loopgo = 0;
+			for (int x = xp; x < xp + w; x++)
+			{
+				cx = (xjump * x) - Math.Abs(minr);
+				for (int y = yp; y < yp + h; y++)
+				{
+					zx = 0;
+					zy = 0;
+					cy = (yjump * y) - Math.Abs(mini);
+					loopgo = 0;
+					while (zx * zx + zy * zy <= 4 && loopgo < loopmax)
+					{
+						loopgo++;
+						tempzx = zx;
+						zx = (zx * zx) - (zy * zy) + cx;
+						zy = (2 * tempzx * zy) + cy;
+					}
+					if (loopgo != loopmax)
+						img.SetPixel(x - xp, y - yp, Color.FromArgb(loopgo % 32 * 7, loopgo % 128 * 2, loopgo % 16 * 14));
+					else
+						img.SetPixel(x - xp, y - yp, Color.Black);
+				}
+			}
+			return img;
+		}
+	}
+}
