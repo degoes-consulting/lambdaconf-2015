28c64f5cccbfdbd45280a570124bc02fbb68fd08
Vincent Marquez <vincent.marquez@gmail.com>
2015-05-21 16:47:17 -0700

    small changes and adding some code

--- speakers/vmarquez/src/main/scala/saveyourstack/Stack.scala
+++ speakers/vmarquez/src/main/scala/saveyourstack/Stack.scala
@@ -3,6 +3,7 @@ package saveyourstack
 import scalaz._
 import Scalaz._
 import org.joda.time._
+import scala.annotation.tailrec
 
 object Dates {
   def generateDates(sd: DateTime, ed: DateTime): List[DateTime] = {
@@ -19,6 +20,33 @@ object Dates {
       else
        l 
     rec(sd, List())
+  }
+}
+
+object MyTrampoline {
+
+  implicit val m: Monad[VTrampoline] = new Monad[VTrampoline] {
+    def point[A](a: => A): VTrampoline[A] = NoMore(a)
+
+    def bind[A,B](vt: VTrampoline[A])(f: A => VTrampoline[B]): VTrampoline[B] = vt.flatMap(f)
+  }
+  
+  case class More[A,B](a: () => VTrampoline[A], f: A => VTrampoline[B]) extends VTrampoline[B]
+  case class NoMore[A](a: A) extends VTrampoline[A]
+
+  trait VTrampoline[A] {
+    def flatMap[B](f: A => VTrampoline[B]): VTrampoline[B] = 
+      More(() => this, f)
+     
+    @tailrec 
+    final def gogo(): A = this match {
+      case NoMore(a) => a
+      case More(ta, f) => ta() match {
+          case NoMore(a) => f(a).gogo
+          case More(tb, ff) => 
+            tb().flatMap(b => ff(b).flatMap(f)).gogo
+        }
+    }
   }
 }
 
@@ -74,27 +102,37 @@ object FileSystemStuff {
 }
 
 object MonadTransformerProblems {
+  import MyTrampoline._
 
   def handleFiles(): Unit = {
     val a = (0 to 10000).map(ii => State[Int,Int](i => (i,ii)) ).foldLeft( State[Int,Int](i => (i,0)) )( (s,a) => s.flatMap(i => a.map(ii => (ii+i) ))) 
     val b = (0 to 10000).map(ii => StateT[Free.Trampoline,Int,Int](i => Trampoline.done((i,ii))) ).foldLeft( StateT[Free.Trampoline, Int,Int](i => Trampoline.done((i,0))) )( (s,a) => s.flatMap(i => a.map(ii => (ii+i) ))) 
     val c = (0 to 10000).map(ii => JDState[Free.Trampoline,Int,Int](Trampoline.done((i:Int) => Trampoline.done((i,ii)))) ).foldLeft( JDState[Free.Trampoline, Int,Int](Trampoline.done(i => Trampoline.done((i,0)))) )( (s,a) => s.flatMap(i => a)) 
+    val e = (0 to 10000).map(ii => liftToJDVT(ii) ).foldLeft( liftToJDVT(1) )( (s,a) => s.flatMap(i => a))
+    
     //a and b will fail. c won't
     val res = c.sf.map(i => i(0)).join.run //(0).run //weird to pull out a bit
     
        val d = (0 to 10000).map(ii => State[Int,Int](i => (i,ii)).liftF ).foldLeft( State[Int,Int](i => (i,0)).liftF )( (s,a) => s.flatMap(i => a.map(ii => (ii+i) )))
            val otherResult = d.foldRun(0)( (a,b) => b(a))
+  
+    (0 to 10000).map(ii => StateT[Free.Trampoline,Int,Int](i => Trampoline.done((i,ii))) ).foldLeft( StateT[Free.Trampoline, Int,Int](i => Trampoline.done((i,0))) )( (s,a) => s.flatMap(i => a.map(ii => (ii+i) ))) 
+
   }
 
-
+  def liftToJDVT[A](a: A): JDState[VTrampoline, A, A] = JDState(NoMore(((aa:A) => NoMore((aa,a)))))
+  
   //Ok John Degoes had a great idea, if F[] is stack safe and we do all our binding *before* we pass a lambda to the state constructor, we're OK 
   case class JDState[F[_], S, A](sf: F[S => F[(S,A)]]) {
     
     def flatMap[B](f: A => JDState[F, S, B])(implicit M: Monad[F]): JDState[F, S, B] = 
       JDState[F, S, B](((s1: S) => { 
-        sf.flatMap(sfa => sfa(s1).flatMap(t =>  
+        sf.flatMap(sfa => { 
+            println("sfa = " + sfa)
+            sfa(s1).flatMap(t =>  
           f(t._2).sf.flatMap(z => z(s1)) 
-        )) 
+          )
+        }) 
       }).point[F])
   }
 
